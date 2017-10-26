8e2d8be4cb104b9789a9d87fdf91f87cbef40cda
Ujwal Setlur <ujwal@ravana.attlocal.net>
2017-10-26 10:51:59 -0700

    Removed duplicate require statements.

--- NavigationHeaderTitle.js
+++ NavigationHeaderTitle.js
@@ -30,7 +30,6 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 const ReactNative = require('react-native');
-const PropTypes = require('prop-types');
 
 const {
   Platform,
